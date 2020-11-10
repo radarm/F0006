e819a98da460e0fc0cdedc81852c532801f05218
radarm <wmtech.radvanszkimate@gmail.com>
2020-11-10 13:14:17 +0000

    Initial commit

---
+++ f0006a.py
@@ -0,0 +1,12 @@
+szam1 = input('Kérem az eslő számot!')
+szam2 = input('Kérem a második számot!')
+
+szam1 = int(szam1) 
+szam2 = int(szam2)
+
+if szam1 < szam2: 
+  kisebb = szam1
+else:
+  kisebb = szam2 
+
+print('kisebb')
